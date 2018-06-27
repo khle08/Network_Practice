bf3cd54cab79e8706832b50ed43d1aac27e3cde4
khle08 <khle08@qq.com>
2018-06-27 12:15:26 +0800

    milestone of simplist image classifier knn

---
+++ pyImageSearch/knn.py
@@ -0,0 +1,9 @@
+from sklearn.neighbors import KneighborsClassifier
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+from pyimagesearch.preprocessing import Simple_Preprocessor
+from pyimagesearch.datasets import Simple_Loader
+from imutils import paths
+import argparse
+
