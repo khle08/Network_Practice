4eef5a6e7f59aad7794f2f4badc51e2385bfee3a
khle08 <khle08@qq.com>
2018-06-28 16:52:19 +0800

    fully complete knn network construction

--- pyImageSearch/knn.py
+++ pyImageSearch/knn.py
@@ -1,4 +1,4 @@
-from sklearn.neighbors import KneighborsClassifier
+from sklearn.neighbors import KNeighborsClassifier
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report
@@ -29,6 +29,20 @@ sdl = Simple_Loader(preprocessors=[sp])
 # show some information on memory consumption of the images
 print("[INFO] features matrix: {:.1f}MB".format(data.nbytes / (1024 * 1000.0)))
 
+
+# encode the labels as integers
+le = LabelEncoder()
+labels = le.fit_transform(labels)
+
+# partition the data into 75% training and 25% testing data
+(trainX, testX, trainY, testY) = train_test_split(data, labels, test_size=0.25, random_state=42)
+
+
+# train and evaluate a k-NN classifier on the raw pixel intensities
+print("[INFO] evaluating k-NN classifier...")
+model = KNeighborsClassifier(n_neighbors=args["neighbors"], n_jobs=args["jobs"])
+model.fit(trainX, trainY)
+print(classification_report(testY, model.predict(testX), target_names=le.classes_))
 
 
 
